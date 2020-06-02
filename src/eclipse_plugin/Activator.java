fc5598a20051d66a8636025befa876d096a1157b
Artsiom Chapialiou <achapialiou@gmail.com>
2020-06-02 21:23:33 +0300

    test plugin

---
+++ src/eclipse_plugin/Activator.java
@@ -0,0 +1,32 @@
+package eclipse_plugin;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import org.eclipse.core.resources.*;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+
+		   IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		   IResourceChangeListener listener = new IResourceChangeListener() {
+		      public void resourceChanged(IResourceChangeEvent event) {
+		         System.out.println("Something changed: " + event);
+		      }
+		   };
+		   workspace.addResourceChangeListener(listener);
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
