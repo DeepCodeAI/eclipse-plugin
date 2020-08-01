4de503c9bc608f7e7a290b47fadd975ca31aa97b
Artsiom Chapialiou <achapialiou@gmail.com>
2020-08-01 13:44:26 +0300

    re-analyze on Settings changed

--- eclipse-plugin/src/ai/deepcode/parts/DeepCodePrefPage.java
+++ eclipse-plugin/src/ai/deepcode/parts/DeepCodePrefPage.java
@@ -5,7 +5,10 @@ import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import ai.deepcode.core.AnalysisData;
 import ai.deepcode.core.DeepCodeParams;
+import ai.deepcode.core.LoginUtils;
+import ai.deepcode.core.RunUtils;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
@@ -29,9 +32,8 @@ public class DeepCodePrefPage extends FieldEditorPreferencePage implements IWork
   protected void createFieldEditors() {
     addField(new StringFieldEditor(BASE_URL, "DeepCode server instance &URL:", getFieldEditorParent()));
     addField(new StringFieldEditor(TOKEN_ID, "&TokenId:", getFieldEditorParent()));
-    addField(new RadioGroupFieldEditor(
-        MIN_SEVERETY_LEVEL, "Min &Severity level to show:", 1, new String[][] {
-            {"&Infos, Warnings and Errors", "infos"}, {"&Warnings and Errors", "warns"}, {"&Errors only", "errors"}},
+    addField(new RadioGroupFieldEditor(MIN_SEVERETY_LEVEL, "Min &Severity level to show:", 1, new String[][] {
+        {"&Infos, Warnings and Errors", "infos"}, {"&Warnings and Errors", "warns"}, {"&Errors only", "errors"}},
         getFieldEditorParent()));
     addField(new BooleanFieldEditor(ADD_LINTERS, "Add &Linters analysis", getFieldEditorParent()));
   }
@@ -40,19 +42,20 @@ public class DeepCodePrefPage extends FieldEditorPreferencePage implements IWork
   public void init(IWorkbench workbench) {
     setPreferenceStore(PREFS_STORE_INSTANCE);
     setDescription("Configuration for the DeepCode plugin");
-    
+
     // read previously saved params
     PREFS_STORE_INSTANCE.setValue(BASE_URL, DeepCodeParams.getInstance().getApiUrl());
     PREFS_STORE_INSTANCE.setValue(TOKEN_ID, DeepCodeParams.getInstance().getSessionToken());
     final int minSeverity = DeepCodeParams.getInstance().getMinSeverity();
-    PREFS_STORE_INSTANCE.setValue(MIN_SEVERETY_LEVEL, 
+    PREFS_STORE_INSTANCE.setValue(MIN_SEVERETY_LEVEL,
         minSeverity == 1 ? "infos" : minSeverity == 2 ? "warns" : minSeverity == 3 ? "errors" : "infos");
     PREFS_STORE_INSTANCE.setValue(ADD_LINTERS, DeepCodeParams.getInstance().useLinter());
-    
+
     // Listener to update DeepCodeParams when changed in GUI
     PREFS_STORE_INSTANCE.addPropertyChangeListener(event -> {
       final String property = event.getProperty();
       final String value = event.getNewValue().toString();
+
       if (property.equals(BASE_URL)) {
         DeepCodeParams.getInstance().setApiUrl(value);
       } else if (property.equals(TOKEN_ID)) {
@@ -61,7 +64,11 @@ public class DeepCodePrefPage extends FieldEditorPreferencePage implements IWork
         int severity = value.equals("infos") ? 1 : value.equals("warns") ? 2 : value.equals("errors") ? 3 : 1;
         DeepCodeParams.getInstance().setMinSeverity(severity);
       } else if (property.equals(ADD_LINTERS)) {
-        DeepCodeParams.getInstance().setUseLinter((Boolean)event.getNewValue());
+        DeepCodeParams.getInstance().setUseLinter((Boolean) event.getNewValue());
+      }
+      AnalysisData.getInstance().resetCachesAndTasks(null);
+      if (LoginUtils.getInstance().isLogged(null, true)) {
+        RunUtils.getInstance().asyncAnalyseProjectAndUpdatePanel(null);
       }
     });
   }
